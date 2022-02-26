1f03f291c2f47c2d4ec018e387b02381fe923eba
radxhunter <jestemrsendecki@gmail.com>
2022-02-26 12:08:33 +0100

    added modbus client and modbus server

---
+++ ModbusStandardLibrary/IModbusStandardService.cs
@@ -0,0 +1,10 @@
+﻿using System.Collections.Generic;
+
+namespace ModbusStandardLibrary
+{
+    public interface IModbusStandardService
+    {
+        IEnumerable<short> GetModbusValues(int startingAddress, int count);
+        void SendModbusRequest(short[] dataSet, int startingAddress);
+    }
+}
