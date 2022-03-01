3f932cdcc9703e41e79450e0d87680cc2c267c6f
radxhunter <jestemrsendecki@gmail.com>
2022-03-01 12:38:16 +0100

    (refactor): organized files, moved projects to /src directory and create the /tests directory

---
+++ src/ModbusServerConsole/Program.cs
@@ -0,0 +1,26 @@
+﻿using EasyModbus;
+using ModbusClientConsole.Helpers;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ModbusServerConsole
+{
+    public delegate void Notify();
+
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Running modbus server...");
+            ModbusServer modbusServer = new ModbusServer();
+            modbusServer.Listen();
+            Console.WriteLine($"Modbus Server is running and has IP Address: { IpLocalizer.GetLocalIPAddress() }... UnitIdentifier is { modbusServer.UnitIdentifier } \r\n Type something to break");
+            //TODO: (5) Create logging functionality: Add infinite loop that handle the event when someone connect to the server, read/write some data etc.; 
+            
+            Console.ReadKey();
+        }
+    }
+}
